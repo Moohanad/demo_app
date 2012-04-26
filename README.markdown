2ef7af7eb0cbf3d98440746bbac565b0d089da46
Moohanad <moohrash@yahoo.com>
2012-04-25 23:29:52 -0700

    trying adding herolu link

---
+++ README.markdown
@@ -0,0 +1,4 @@
+Hi!
+This the demo app from the Rails 3 Tutorial.
+I deployed the app the Heroku, this is the URL:
+http://radiant-water-3527.heroku.com/users/new
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'bdd331de2657ffd4d9f7120537e004ef07e3a7a6b9385f8916ced95f67ddbfc3e1c0c8d961aa960bdffbcbeefdffe7a517e9f1ff26783ff47412c3b5248c33d7'
